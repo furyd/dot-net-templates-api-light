567f2fde15da08567583ca2892cf0500fe9d6b03
Dave Fury <david.fury@reservoir-devs.com>
2020-05-18 15:26:43 +0100

    feat: added example exception

---
+++ src/Template.Exceptions/ExampleException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Template.Exceptions
+{
+    [Serializable]
+    public class ExampleException : Exception
+    {
+        public string ExampleProperty { get; set; }
+
+        public ExampleException()
+        {
+        }
+
+        public ExampleException(string message) : base(message)
+        {
+        }
+
+        public ExampleException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected ExampleException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            ExampleProperty = info.GetString(nameof(ExampleProperty));
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(ExampleProperty), ExampleProperty);
+            base.GetObjectData(info, context);
+        }
+    }
+}
